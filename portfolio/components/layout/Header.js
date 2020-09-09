13516748f97223e71741fd8bb3bceada66f1ef08
Patrick Shushereba <pshushereba@gmail.com>
2020-09-08 17:23:22 -0700

    Added projects to home page.

--- portfolio/components/layout/Header.js
+++ portfolio/components/layout/Header.js
@@ -2,20 +2,18 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="bg-red-700 w-full h-16">
-      <div className="flex align-middle h-full justify-between">
-        <div className="w-1/5 flex align-middle">
-          <h1 className="h-full self-center">Patrick Shushereba</h1>
-        </div>
-        <div className="flex align-middle w-2/5 justify-around">
+    <div className="w-full h-16 bg-bg-gray-custom">
+      <div className="flex align-middle justify-between">
+        <a className="h-full text-white w-1/5 self-center text-center">
+          Patrick Shushereba
+        </a>
+
+        <div className="flex align-middle w-1/5 justify-evenly pr-16">
           <Link href="/about">
-            <a className="m-4">About</a>
-          </Link>
-          <Link href="/projects">
-            <a className="m-4">Projects</a>
+            <a className="m-4 text-white">About</a>
           </Link>
           <Link href="/blog">
-            <a className="m-4">Blog</a>
+            <a className="m-4 text-white">Blog</a>
           </Link>
         </div>
       </div>
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,13 +1,103 @@
 import Layout from "../components/layout/index.js";
+import SkylineSVG from "../components/svg/SkylineSVG.js";
 
 const Index = ({ title, description, ...props }) => {
   return (
-    <div>
-      <Layout>
-        <p className="text-lg">Hello, world!</p>
-      </Layout>
-    </div>
+    <>
+      <Layout />
+      <section className="hero-top">
+        <div className="bg-bg-gray-custom pt-12 pb-12">
+          <div className="flex justify-around">
+            <div className="w-2/5">
+              <SkylineSVG />
+            </div>
+            <div className="w-2/5 self-center">
+              <p className="text-white w-3/5 leading-relaxed text-xl">
+                I am a detail oriented full stack web developer with a research
+                background. When I'm not developing, you can find me reading,
+                deep diving on new programming technologies, or running half
+                marathons.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="projects-section bg-bg-gray-custom">
+        <p className="text-white">My work</p>
+        <div className="flex justify-around">
+          <div className="w-1/3">
+            <div className="w-3/4 pb-4">
+              <img src="/nutrivurv.png" className="max-w-md h-56" />
+            </div>
+            <div className="w-3/4">
+              <p className="text-white">
+                Nutrivurv is an application that makes it easy for you to track
+                your health and fitness. It features a way to track all of your
+                food in a journal. We use the Edamam API to return all of the
+                data. It also allows you to update your weight so that you can
+                see your progress over time. Finally, it includes a blog so that
+                users can share their progress.
+              </p>
+            </div>
+          </div>
+          <div className="w-1/3">
+            <div className="w-3/4 pb-4">
+              <img src="/spider_graph_chart.png" className="max-w-md h-56" />
+            </div>
+            <div className="w-3/4">
+              <p className="text-white">
+                Spider Graph is an application that makes it easy for users to
+                create graphs of their data. They are able to add as many axis
+                as they need to present their data. The back end consists of a
+                REST API built using Node, Express, Knex and Postgres. Deployed
+                to Heroku.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
 export default Index;
+
+{
+  /* <div className="bg-bg-gray-custom">
+
+<p>
+  Here are some of the projects that I've been working on. You can see
+  the code as well as the deployed project.
+</p>
+<div className="flex justify-around">
+  <div className="flex-col">
+    <div className="w-1/3">
+      <img src="/nutrivurv.png" />
+    </div>
+    <div className="w-1/3 flex items-center">
+      <p className="text-white">
+        Nutrivurv is an application that makes it easy for you to
+        track your health and fitness. It features a way to track all
+        of your food in a journal. We use the Edamam API to return all
+        of the data. It also allows you to update your weight so that
+        you can see your progress over time. Finally, it includes a
+        blog so that users can share their progress.
+      </p>
+    </div>
+  </div>
+</div>
+
+<div className="w-1/3">
+  <img src="/spider_graph_chart.png" />
+</div>
+<div className="w-1/3 flex">
+  <p className="text-white">
+    Spider Graph is an application that makes it easy for users to
+    create graphs of their data. They are able to add as many axis as
+    they need to present their data. The back end consists of a REST
+    API built using Node, Express, Knex and Postgres. Deployed to
+    Heroku.
+  </p>
+</div>
+</div> */
+}
